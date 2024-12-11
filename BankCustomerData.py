fec0accf4934da23219b65ebf63f2c9aa4c14a2b
Pulkit M <141311487+m-pulkit@users.noreply.github.com>
2024-12-12 00:56:24 +0100

    Added example to class
    For displaying in FastAPI docs

--- BankCustomerData.py
+++ BankCustomerData.py
@@ -11,3 +11,21 @@ class BankCustomerData(BaseModel):
     Loyalty: float
     Geography_Germany: int
     Geography_Spain: int
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                  "CreditScore": 0.658,
+                  "Balance": 0.30512014,
+                  "NumOfProducts": 0,
+                  "HasCrCard": 1,
+                  "IsActiveMember": 0,
+                  "EstimatedSalary": 0.9240317 ,
+                  "Loyalty": 0.20769231,
+                  "Geography_Germany": 1,
+                  "Geography_Spain": 0
+                }
+            ]
+        }
+    }
