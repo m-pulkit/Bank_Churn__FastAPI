711f67b3bcb654995e15a4bed9f5449a63ffdf37
Pulkit M <141311487+m-pulkit@users.noreply.github.com>
2024-12-12 23:57:28 +0100

    Update app.py
    Render demands host as 0.0.0.0

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def predict_churn(data: BankCustomerData):
 
 # Run the API with uvicorn
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=4000)
 
 
 '''
